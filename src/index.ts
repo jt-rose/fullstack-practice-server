47454a85f71748817af74c2694e635b0a7bfefd7
Jeffrey Rose <jtr219@outlook.com>
2020-12-26 14:01:59 -0500

    set up basic server

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 5000
+/*
+interface Monster {
+    name: string;
+    location: string;
+    hobbies: string;
+}
+
+
+const mockDB: Monster[] = [
+    {
+        name: "Dracula",
+        location: "Transylvania",
+        hobbies: "sucking blood"
+    },
+    {
+        name: "Zombie",
+        location: "RCPD station",
+        hobbies: "eating brains"
+    },
+    {
+        name: "Cthulu",
+        location: "Murky depths",
+        hobbies: "spelunking"
+    }
+]
+*/
+
+app.get("/hellonode", (_req, res) => {
+    res.json({
+        text: "hello Node"
+    })
+})
+
+app.listen(port, () => {
+    console.log(
+        `listening on port ${port}`
+    )
+})
